110d0d9c80afb4655de85d51fc2fe0a42d43eb34
Ben Kwan <benkwan26@gmail.com>
2026-03-21 16:06:55 -0400

    Add parameter tuning configuration

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,11 +1,14 @@
-import pandas as pd
 import numpy as np
 import os
+import pandas as pd
 from sklearn.model_selection import train_test_split
+import yaml
+
+TEST_SIZE = yaml.safe_load(open('params.yaml'))['data_collection']['test_size']
 
 df = pd.read_csv("./data/water_potability.csv")
 
-train_data, test_data = train_test_split(df, test_size=0.2, random_state=42)
+train_data, test_data = train_test_split(df, test_size=TEST_SIZE, random_state=42)
 
 DATA_PATH = os.path.join('data', 'raw')
 os.makedirs(DATA_PATH)
--- src/model_building.py
+++ src/model_building.py
@@ -1,15 +1,18 @@
-import pandas as pd
 import numpy as np
 import os
+import pandas as pd
 import pickle
 from sklearn.ensemble import RandomForestClassifier
+import yaml
+
+N_ESTIMATORS = yaml.safe_load(open('params.yaml'))['model_building']['n_estimators']
 
 train_df = pd.read_csv("./data/processed/train_processed.csv")
 
 X_train = train_df.drop(columns=["Potability"])
 y_train = train_df['Potability']
 
-clf = RandomForestClassifier()
+clf = RandomForestClassifier(n_estimators=N_ESTIMATORS)
 clf.fit(X_train, y_train)
 
 pickle.dump(clf, open('model.pkl', 'wb'))
